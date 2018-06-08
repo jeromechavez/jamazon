8f2363eea49e9a524240f5b2230364205ea3755a
Jerome Chavez <jayrome12@yahoo.com>
2018-06-08 15:49:29 -0700

    Created event listener to listen for change events on the select elements and re-renders the app state with a sorted catalog

--- main.js
+++ main.js
@@ -331,24 +331,21 @@ function renderPriceSort() {
   var dropDownContainer = document.createElement('div')
   dropDownContainer.classList.add('form-group', 'w-25')
 
-  var dropDownLabel = document.createElement('label')
-  dropDownLabel.textContent = 'Sort By:'
-  dropDownContainer.appendChild(dropDownLabel)
-
   var dropDownGroup = document.createElement('select')
   dropDownGroup.classList.add('form-control')
+  dropDownGroup.setAttribute('name', 'drop-down-sort')
 
   var allView = document.createElement('option')
-  allView.textContent = 'All'
+  allView.textContent = 'Sort By'
   dropDownGroup.appendChild(allView)
-  allView.setAttribute('id', 'unsorted')
+  allView.setAttribute('value', 'unsorted')
   var lowToHighOpt = document.createElement('option')
   lowToHighOpt.textContent = 'Price: Low to High'
-  lowToHighOpt.setAttribute('id', 'low-high')
+  lowToHighOpt.setAttribute('value', 'low-high')
 
   var highToLowOpt = document.createElement('option')
   highToLowOpt.textContent = 'Price: High to Low'
-  highToLowOpt.setAttribute('id', 'high-low')
+  highToLowOpt.setAttribute('value', 'high-low')
 
   dropDownGroup.appendChild(lowToHighOpt)
   dropDownGroup.appendChild(highToLowOpt)
@@ -357,6 +354,16 @@ function renderPriceSort() {
   dropDownForm.appendChild(dropDownContainer)
 
   return dropDownForm
+}
+
+function sortPrice(app, number) {
+  if (number === 0) {
+    app.catalog.items = app.catalog.items.sort(compareValues('price'))
+  }
+  else if (number === 1) {
+    app.catalog.items = app.catalog.items.sort(compareValues('price', 'desc'))
+  }
+  return app
 }
 
 function renderItemDetails(item) {
@@ -513,13 +520,26 @@ renderAppState(app)
 var $catalogView = document.querySelector("[data-view='catalog']")
 $catalogView.addEventListener('click', (event) => {
   var $closestItem = event.target.closest('.card')
-  var itemClicked = parseInt($closestItem.dataset.itemId, 16)
   if ($closestItem) {
+    var itemClicked = parseInt($closestItem.dataset.itemId, 16)
     app.view = 'details'
     app.details.item = getObject(app.catalog.items, itemClicked)
     renderAppState(app)
   }
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector("[data-view='catalog']").onchange = changeEventHandler
+}, false)
+
+function changeEventHandler(event) {
+  if (event.target.value === 'low-high') {
+    renderAppState(sortPrice(app, 0))
+  }
+  else if (event.target.value === 'high-low') {
+    renderAppState(sortPrice(app, 1))
+  }
+}
 
 var $cartSummaryView = document.querySelector('.container-cart')
 $cartSummaryView.addEventListener('click', (event) => {
